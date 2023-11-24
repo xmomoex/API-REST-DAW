95da96b0579e745d076d7c5d2aeb705f15022523
xmomoex <moimorex@gmail.com>
2023-11-24 19:29:28 +0100

    1.rama master

---
+++ index.js
@@ -0,0 +1,56 @@
+// Importamos el framework express.
+const express = require("express");
+
+// Inicializamos la aplicación
+const app = express();
+
+// Indicamos que la aplicación puede recibir JSON (API Rest)
+app.use(express.json());
+
+// Indicamos el puerto en el que vamos a desplegar la aplicación
+const port = process.env.PORT || 8080;
+
+// Arrancamos la aplicación
+app.listen(port, () => {
+  console.log(`Servidor desplegado en puerto: ${port}`);
+});
+
+// Definimos una estructura de datos
+// (temporal hasta incorporar una base de datos)
+let coches = [
+  { marca: "Renault", modelo: "Clio" },
+  { marca: "Nissan", modelo: "Skyline R34" },
+];
+
+// Lista todos los coches
+app.get("/coches", (request, response) => {
+  response.json(coches);
+});
+
+// Añadir un nuevo coche
+app.post("/coches", (request, response) => {
+  coches.push(request.body);
+  response.json({ message: "ok" });
+});
+
+// Obtener un solo coche
+app.get("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  const result = coches[id];
+  response.json({ result });
+});
+
+// Actualizar un solo coche
+app.put("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  coches[id] = request.body;
+  response.json({ message: "ok" });
+});
+
+// Borrar un elemento del array
+app.delete("/coches/:id", (request, response) => {
+  const id = request.params.id;
+  coches = coches.filter((item) => coches.indexOf(item) !== id);
+
+  response.json({ message: "ok" });
+});
